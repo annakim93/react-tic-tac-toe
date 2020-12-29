e2f1e2456fe26454a88eeb39f937c882a36245b4
Anna Kim <anna.kim93@gmail.com>
2020-12-29 16:33:08 -0500

    added function onButtonClick to Square

--- src/components/Square.js
+++ src/components/Square.js
@@ -4,13 +4,13 @@ import PropTypes from 'prop-types';
 import './Square.css'
 
 const Square = (props) => {
-  // For Wave 1 enable this 
-  //  Component to alert a parent 
-  //  component when it's clicked on.
+  const onButtonClick = () => {
+    props.onClickCallback(props.id);
+  };
 
   return <button
     className="square"
-    onClick={props.onClickCallback}
+    onClick={onButtonClick}
   >
     {props.value}
   </button>
