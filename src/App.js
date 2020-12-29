390071c8a8fae19075196c2757c017a440d05d06
Anna Kim <anna.kim93@gmail.com>
2020-12-29 17:33:15 -0500

    add logic checkForWinner - check rows, cols, diagonals

--- src/App.js
+++ src/App.js
@@ -31,11 +31,8 @@ const App = () => {
   // empty value and unique ids.
   const [squares, setSquares] = useState(generateSquares());
   const [player, setPlayer] = useState(PLAYER_1);
+  const [winner, setWinner] = useState(false);
 
-  // Wave 2
-  // You will need to create a method to change the square 
-  //   When it is clicked on.
-  //   Then pass it into the squares as a callback
   const updateSquares = (id) => {
     const squareCopy = [];
     let currentId = 0;
@@ -53,7 +50,7 @@ const App = () => {
           } else {
             setPlayer(PLAYER_1)
           }
-          
+
         } else {
           squareCopy[row].push(squares[row][col]);
         }
@@ -61,20 +58,56 @@ const App = () => {
       }
     }
     
-    setSquares(squareCopy)
+    setSquares(squareCopy);
+    let newWinner = checkForWinner(squareCopy);
+    if (newWinner !== false) {
+      setWinner(newWinner);
+    }
   }
 
-  const checkForWinner = () => {
-    // Complete in Wave 3
-    // You will need to:
-    // 1. Go accross each row to see if 
-    //    3 squares in the same row match
-    //    i.e. same value
-    // 2. Go down each column to see if
-    //    3 squares in each column match
-    // 3. Go across each diagonal to see if 
-    //    all three squares have the same value.
+  const checkForWinner = (squares) => {
+    // First check rows:
+    for (let row of squares) {
+      if (row.every(obj => obj.value ==='X')) {
+        return PLAYER_1;
+      } else if (row.every(obj => obj.value ==='O')) {
+        return PLAYER_2;
+      }
+    }
 
+    // Check cols:
+    for (let col = 0; col < 3; col += 1) {
+      if (squares[0][col].value === 'X' && 
+          squares[0][col].value === squares[1][col].value && 
+          squares[1][col].value === squares[2][col].value) {
+        return PLAYER_1;
+      } else if (squares[0][col].value === 'O' && 
+      squares[0][col].value === squares[1][col].value && 
+      squares[1][col].value === squares[2][col].value) {
+        return PLAYER_2;
+      }
+    }
+
+    // Check diagonals:
+    if (squares[0][0].value === 'X' && 
+        squares[0][0].value === squares[1][1].value && 
+        squares[1][1].value === squares[2][2].value) {
+      return PLAYER_1;
+    } else if (squares[0][0].value === 'O' && 
+               squares[0][0].value === squares[1][1].value && 
+               squares[1][1].value === squares[2][2].value) {
+      return PLAYER_2;
+    } else if (squares[0][2].value === 'X' && 
+               squares[0][2].value === squares[1][1].value && 
+               squares[1][1].value === squares[2][0].value) {
+      return PLAYER_1;
+    } else if (squares[0][2].value === 'O' && 
+               squares[0][2].value === squares[1][1].value && 
+               squares[1][1].value === squares[2][0].value) {
+      return PLAYER_2;
+    }
+
+    return false;
   }
 
   const resetGame = () => {
@@ -85,7 +118,7 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <h1>React Tic Tac Toe</h1>
-        <h2>The winner is ... -- Fill in for wave 3 </h2>
+        <h2>{winner === false ? '' : `The winner is ${winner}`}</h2>
         <button>Reset Game</button>
       </header>
       <main>
